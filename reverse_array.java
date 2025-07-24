0d67f5fa2960dff0f1838ba4041e2ce63f7812a5
Barath2608 <sbbarathkumar@gmail.com>
2025-07-24 17:33:12 +0530

    reverse_array.java

--- reverse_array.java
+++ reverse_array.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class ex {
+public class reverse_array {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter no of array elements: ");
